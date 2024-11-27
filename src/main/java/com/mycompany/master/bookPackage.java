327baecebeb5ce47a39f13fbd707fbdd74eb2a77
DarrelBaylon <darrelandrewb@gmail.com>
2024-11-27 20:02:39 +0800

    Update bookPackage.java

--- src/main/java/com/mycompany/master/bookPackage.java
+++ src/main/java/com/mycompany/master/bookPackage.java
@@ -350,6 +350,7 @@ public class bookPackage extends JFrame implements ActionListener {
         int book = JOptionPane.showConfirmDialog(bookPackage.this,"Are you sure you want to book now?","Confirm Booking",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
 
         if (book == JOptionPane.YES_OPTION) {
+            new BookingFormTwo(firstName,lastName,userID,phoneNumber);
             dispose();
     }
         
